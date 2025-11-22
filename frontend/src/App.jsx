cd1b8eeba43dc01c15c4b26b5959916d5b3574d3
Piyush6949 <rautp6949@gmail.com>
2025-11-22 13:36:15 +0530

    price chart and demand chart complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,17 @@ import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from "r
 function App() {
   const [query, setQuery] = useState("");
   const [summary, setSummary] = useState("");
+  const [price_trend,setPt] = useState([]);
+  const [demandTrend, setDemandTrend] = useState([]);
+
 
   const handleAnalyze = async () => {
     const response = await fetch("http://localhost:8000/api/analyze/?area=" + query);
     const result = await response.json();
+    // const priceTrend = result.price_trend;
+    setPt(result.price_trend);
     setSummary(result.summary);
+    setDemandTrend(result.demand_trend);
   };
 
   return (
@@ -30,7 +36,43 @@ function App() {
           <p>{summary}</p>
         </div>
       )}
-     
+      {price_trend.length > 0 && (
+        <div className="mt-4">
+          <h4>Price Trend</h4>
+          <LineChart
+            width={600}
+            height={300}
+            data={price_trend}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="year" />
+            <YAxis tickFormatter={(value) => (value / 10000000).toFixed(1) + " Cr"} />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="total_sales - igr" stroke="#8884d8" />
+          </LineChart>
+        </div>
+      )}
+      {demandTrend.length > 0 && (
+  <div className="mt-4">
+    <h4>Demand Trend</h4>
+
+    <LineChart
+      width={600}
+      height={300}
+      data={demandTrend}
+      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+    >
+      <CartesianGrid stroke="#ccc" />
+      <XAxis dataKey="year" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="total sold - igr" stroke="#82ca9d" />
+    </LineChart>
+  </div>
+)}
 
 
     </div>
